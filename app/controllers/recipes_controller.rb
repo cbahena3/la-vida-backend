fbcb83b90878aeaa4c8569a79ba0e29887cf441e
Cristian B <cris.bahena77@gmail.com>
2024-03-09 11:47:41 -0600

    added create, update, destroy actions (no authentication just mvp)

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,4 +7,34 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find_by(id: params[:id])
     render :show
   end
+  def create
+    @recipe = Recipe.create(
+      name: params[:name],
+      image: params[:image],
+      description: params[:description],
+      ingredients: params[:ingredients],
+      time: params[:time],
+      user_id: params[:user_id],
+      servings: params[:servings]
+    )
+    render :show
+  end
+  def update
+    @recipe = Recipe.find_by(id: params[:id])
+    @recipe.update(
+      name: params[:name] || @recipe.name ,
+      image: params[:image] || @recipe.image ,
+      description: params[:description] || @recipe.description ,
+      ingredients: params[:ingredients] || @recipe.ingredients ,
+      time: params[:time] || @recipe.time ,
+      user_id: params[:user_id] || @recipe.user_id ,
+      servings: params[:servings] || @recipe.servings
+    )
+    render :show
+  end
+  def destroy
+    @recipe = Recipe.find_by(id: params[:id])
+    @recipe.destroy
+    render json:{message: "recipe deleted"}
+  end
 end
--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -8,4 +8,31 @@ class RecipesControllerTest < ActionDispatch::IntegrationTest
   #   data = JSON.parse(response.body)
   #   assert_equal Recipe.count, data.length
   # end
+  # test "show" do
+  #   get "/recipes/#{Recipe.first.id}.json"
+  #   assert_response 200
+
+  #   data = JSON.parse(response.body)
+  #   assert_equal ["id", "name", "width", "height", "created_at", "updated_at"], data.keys
+  # end
+  # test "create" do
+  #   assert_difference "Recipe.count", 1 do
+  #     post "/recipes.json", params: { name: "lake", width: 800, height: 600 }
+  #     assert_response 200
+  #   end
+  # end
+  # test "update" do
+  #   recipe = Recipe.first
+  #   patch "/recipes/#{recipe.id}.json", params: { name: "Updated name" }
+  #   assert_response 200
+
+  #   data = JSON.parse(response.body)
+  #   assert_equal "Updated name", data["name"]
+  # end
+  # test "destroy" do
+  #   assert_difference "Recipe.count", -1 do
+  #     delete "/recipes/#{Recipe.first.id}.json"
+  #     assert_response 200
+  #   end
+  # end
 end
